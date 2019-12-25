14d4e3b9a2bf6dd9d6ecf1aa8c798edb4e9390ab
Sunil Karki <karkisunil1200@gmail.com>
2019-12-25 16:29:57 -0600

    Displaying json objects

--- index.js
+++ index.js
@@ -32,9 +32,15 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === '/' || pathName === '/overview') {
-    res.end('this is the overview');
-  } else if (pathName == '/product') {
-    res.end('This is the Product');
+    res.end('this is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else if (pathName == '/api') {
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, {'Content-type': 'application/json'});
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
